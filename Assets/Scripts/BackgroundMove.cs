57e8da6c23d766f5420ab74bad02ea2413c23911
Roy Alva <ralvac24@gmail.com>
2023-05-07 21:31:38 -0500

    Added Background Parallax

---
+++ Assets/Scripts/BackgroundMove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMove : MonoBehaviour
+{
+    private MeshRenderer meshRenderer;
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        meshRenderer.material.mainTextureOffset = 
+            new Vector2(0, meshRenderer.material.mainTextureOffset.y + Time.deltaTime * speed);
+    }
+}
